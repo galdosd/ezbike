1cf6ca0b2e6fe6530d2a550901c6a41ec16d9d1e
evabrandenburg <evabrandenburg89@gmail.com>
2014-05-04 18:48:58 -0400

    js doesn't do anything yet

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,29 @@
-/* CURRENTLY IN: javascript/main.js */
+(function(){
+
+$(document).ready(function(){
+  /*$('.bxslider').bxSlider({
+  	minSlides: 1,
+  	maxSlides: 1,
+  	slideWidth: 350,
+  	slideMargin: 10
+	});*/
+
+var go = $( '#go' );
+var api = 'http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?home_latitude=5.5&home_longitude=0&work_latitude=40.714&work_longitude=-73.989'
+
+go.click(
+	function( e ) {
+
+		$.get(
+			api
+			, function( data ) {
+  				
+			});
+
+
+
+	});
+});
+
+
+})();
