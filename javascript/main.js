ed68310418fba30f15182dc3f9b3d8e02342b275
evabrandenburg <evabrandenburg89@gmail.com>
2014-05-04 19:44:29 -0400

    api added to js

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,48 @@
 (function(){
 
-$(document).ready(function(){
-  /*$('.bxslider').bxSlider({
-  	minSlides: 1,
-  	maxSlides: 1,
-  	slideWidth: 350,
-  	slideMargin: 10
-	});*/
+	$(document).ready(function(){
 
-var go = $( '#go' );
-var api = 'http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?home_latitude=5.5&home_longitude=0&work_latitude=40.714&work_longitude=-73.989'
-
-go.click(
-	function( e ) {
-
-		$.get(
-			api
-			, function( data ) {
-  				
-			});
+	/*useLo = $( '#useLo' );
+	Go = $( '#go' );
+	Go.on(
+	'click'
+	, function( e ) {
+		
+			// grab the task in the input field
+			var start = $( '#start' ).val();
 
 
+			if ( start == "" || start == " " ) {
+				alert( 'where you at?' );
+				e.stopPropagation();
+				//done
+				return;
+			}
+
+			var start = $( '#finish' ).val();
+
+
+			if ( start == "" || start == " " ) {
+				alert( 'where you at?' );
+				e.stopPropagation();
+				//done
+				return;
+			}
+
+      	// if we made it here, then valid
+      	var end2 = $( '.end2' );
+      	end2.text( finalChoice2 );	
+      }
+      );
+
+    */
+    	jQuery.getJSON( "http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?home_latitude=5.5&home_longitude=0&work_latitude=40.714&work_longitude=-73.989",
+    		 function ( data ) {
+    		 	console.log( 'data' );
+    		 }
+    	) 
 
 	});
-});
 
 
 })();
