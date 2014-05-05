a2022408f894883582c48005533bd5935b670563
evabrandenburg <evabrandenburg89@gmail.com>
2014-05-04 20:55:58 -0400

    more?

--- javascript/main.js
+++ javascript/main.js
@@ -34,26 +34,29 @@
       	end2.text( finalChoice2 );	
       }
       );
-
+		
     */
+
     	jQuery.getJSON( "http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?"
     		+"home_latitude=5.5"
     		+"&home_longitude=0"
     		+"&work_latitude=40.714"
     		+"&work_longitude=-73.989",
     		 function ( data ) {
-    		 	var startC = $( '.startChoices');
+    		 	var start1 = $( '.start1' );
+    		 	var start2 = $( '.start2' );
+    		 	var start3 = $( '.start3' );
     		 	var tooFar = data.begin_at[0].blocks
     		 	var warning = $( '.warning');
-    		 	startC.text(
+    		 	start1.text(
     		 		"Begin at "
     		 		+(data.begin_at[0].name) 
-    		 		+ ( " , " )
+    		 		+ ( ", " )
     		 		+ ( data.begin_at[0].blocks ) 
     		 		+ " blocks away"
     		 	);
     		 	if(tooFar > 20) {
-    		 		warning.text("maybe you shouldn't bike");
+    		 		warning.text("That's pretty far bro, maybe you shouldn't bike");
     		 	}
 
     		 }
