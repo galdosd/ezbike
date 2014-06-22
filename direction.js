07be46d8cc1fbf0680edd2bbb4f9233a31dfb494
evabrandenburg <evabrandenburg89@gmail.com>
2014-06-22 13:32:55 -0400

    maybe it works?

--- direction.js
+++ direction.js
@@ -1,52 +1,58 @@
-
 /*function output( number ) {
 
 };*/
-var latitude;
-var longitude;
+
 
 
 //http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?home_latitude=5.5&home_longitude=0&work_latitude=40.714&work_longitude=-73.989
-var click = $( "#click" );
+var Go = $( "#go" );
 var where = $( "#where" );
+var work = $( "#work" );
 
-click.click(function(){
-	navigator.geolocation.getCurrentPosition(function(position) {
+Go.click( callNav );
+
+function callNav(){
+	navigator.geolocation.getCurrentPosition(function( position ) {
 		var latitude = position.coords.latitude;
 		var longitude = position.coords.longitude;
 		where.text( "The latitude is "
 		 + latitude
 		  + " and the longitude is " + longitude + " good luck");
 
-var work = $( "#work" );
-
-	work.click(function(){
-	 	$.getJSON( "http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?"
-			+"home_latitude="
-			+latitude
-			+"&home_longitude="
-			+longitude
-			+"&work_latitude=55.714"
-			+"&work_longitude=-65.989",
-			 $.each(results[0].address_components, function(){
-             $("#start").find('input[name="'+ this.types+'"]').attr('value', this.long_name);
-            };
-	    function work( data ) {
-	    	var start0 = $( '#start0' );
-	    	start0.text(
-	    		 data.begin_at[0].slots
-	    		 + " bikes at "
-	    		 + data.begin_at[0].name  
-	    		 + ", " 
-	    		 + data.begin_at[0].blocks  
-	    		 + " blocks away"
-	    	);
-	    };
+		callApi( latitude, longitude );
 	}); 
+};
 
 
-	});
-});
+function callApi( hLatitude, hLongitude ){
+ 	$.getJSON( "http://ezbike.xweb.service.cmwp.com/cgi-bin/report.pl?"
+		+"home_latitude="
+		+hLatitude
+		+"&home_longitude="
+		+hLongitude
+		+"&work_latitude=55.714"
+		+"&work_longitude=-65.989", answer
+    );
+};
+
+
+function answer( data ) {
+	var start0 = $( '#start0' );
+	start0.text(
+		 data.begin_at[0].slots
+		 + " bikes at "
+		 + data.begin_at[0].name  
+		 + ", " 
+		 + data.begin_at[0].blocks  
+		 + " blocks away"
+	);
+};
+
+/*Go.click(function GO(){
+	callNav();
+	callApi();
+	answer( data );
+});*/
 
 //already able to generate lat and long from location on click
 //need to put that lat and long into API web address
@@ -56,3 +62,4 @@ var work = $( "#work" );
 
 /*button.click(function(){
 		where.text( latitude );*/
+
