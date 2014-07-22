1598709262fcf66c808e616ec9fb814587839cb6
evabrandenburg <evabrandenburg89@gmail.com>
2014-07-22 19:07:58 -0400

    moer

--- direction.js
+++ direction.js
@@ -115,10 +115,11 @@ function callApi( hLatitude, hLongitude ){
 			data = $.parseJSON(data);
 			answer(data);
 			console.log( "answer" );
+		},
+		complete: function(data) {
+			console.log(data)
+			console.log( "complete" );
 		}
-		//complete: function(data) {
-		//	console.log(data)
-		//}
     });
 };
 
